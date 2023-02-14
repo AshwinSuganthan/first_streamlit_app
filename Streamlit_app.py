1569939f257687a4dfe93021c2f69914f80e68a3
AshwinSuganthan <124761880+AshwinSuganthan@users.noreply.github.com>
2023-02-14 06:28:24 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title ('Ashwin Snowflake badge 2')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
