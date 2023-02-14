f94c33d2d0671d38e06b7c56a8b39e86cfb4a167
AshwinSuganthan <124761880+AshwinSuganthan@users.noreply.github.com>
2023-02-14 06:48:40 +0530

    Update PANDAS.py

--- PANDAS.py
+++ PANDAS.py
@@ -5,6 +5,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 streamlit.dataframe(my_fruit_list)
 
-streamlit.multiselect("pick some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("pick some Fruits:", list(my_fruit_list.index),['Pear','Peach'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
