b15ffee08ef67cbffd8f2effb7ac8bdbbef40fe9
AshwinSuganthan <124761880+AshwinSuganthan@users.noreply.github.com>
2023-02-14 06:52:05 +0530

    Update PANDAS.py

--- PANDAS.py
+++ PANDAS.py
@@ -3,8 +3,9 @@ import streamlit
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.dataframe(my_fruit_list)
-
 fruits_selected = streamlit.multiselect("pick some Fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(my_fruit_list)
+
+
 
