ba74d87a6f7389df7495eef7d643e61e56123207
AshwinSuganthan <124761880+AshwinSuganthan@users.noreply.github.com>
2023-02-16 19:18:18 +0530

    Update PANDAS.py

--- PANDAS.py
+++ PANDAS.py
@@ -1,5 +1,6 @@
 import pandas
 import streamlit
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -7,5 +8,8 @@ fruits_selected = streamlit.multiselect("pick some Fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
